8919fe617117d88229f6c2262b37c7371a0a632e
rifky <rifkyhilman265@gmail.com>
2019-08-30 23:12:16 +0700

    new ejs

--- routes.js
+++ routes.js
@@ -10,7 +10,6 @@ routes.get('/tutorial' , (req,res) => {
         if(error){
             console.log(error)
         }else{
-            console.log(rows)
             res.render('pages/tutorial.ejs', {data: rows})
         }
 
@@ -22,11 +21,148 @@ routes.get('/tentang' , (req,res) => {
 })
 
 routes.get('/register' , (req,res) => {
-    res.render('pages/register.ejs')
+    if(res.locals.kunci) {
+        res.redirect('/')
+    } else {
+        res.render('pages/register.ejs', {err: false})
+    }
 })
 
 routes.get('/signin' , (req,res) => {
-    res.render('pages/signin.ejs')
+    if(res.locals.kunci) {
+        res.redirect('/')
+    } else {
+        res.render('pages/signin.ejs', {err : false})
+    }
+})
+
+routes.post('/proses-signin' , (req,res) => {
+    console.log(req.body)
+    connection.query('SELECT * FROM user WHERE username= ? AND password= ?',
+        [ req.body.username, req.body.password ] ,
+        function (error, rows, field) {
+            if(error) {
+                res.render('pages/signin.ejs', {err: 'lognin gagal'})
+            } else{
+                if(rows.length > 0) {
+                    req.session.kunci = {
+                        username: rows[0].username
+                    }
+                    res.redirect('/')
+                } else{
+                    res.render('pages/signin.ejs', {err: 'password atau username salah'})
+                }
+            }
+        });
+})
+
+
+routes.get('/logout' , (req,res) => {
+    if (req.session) {
+        // delete session object
+        req.session.destroy(function(err) {
+          if(err) {
+            return next(err);
+          } else {
+            return res.redirect('/');
+          }
+        });
+      }
+})
+
+routes.get('/add-blog' , (req,res) => {
+    res.render('pages/add-blog.ejs')
+})
+routes.get('/home2' , (req,res) => {
+    res.render('pages/home2.ejs')
+})
+
+routes.get('/kategoris' , (req,res) => {
+    connection.query('SELECT * FROM kategori', function (error, rows, field){
+        if(error){
+            console.log(error)
+        }else{
+            console.log(rows)
+            res.render('pages/kategoris.ejs', {data: rows})
+        }
+
+    }) ;
+})
+
+routes.post('/save-blog' , (req,res) => {
+
+    const created_at = new Date();
+
+    connection.query('INSERT INTO blogs (title, content, created_at) values (?,?,?)', 
+    [ req.body.title, req.body.content, created_at ], 
+    function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            res.redirect('/tutorial')
+        }
+    });
+})
+
+routes.post('/proses-register' , (req,res) => {
+
+    if(req.body.password === req.body.Repeatpsw) {
+        connection.query('INSERT INTO user (username, password) values (?,?)',
+        [ req.body.username, req.body.password ] ,
+        function (error, rows, field) {
+            if(error) {
+                console.log(error)
+            } else{
+                res.redirect('/signin')
+            }
+        });
+    } else {
+        res.render('pages/register.ejs', { err: 'Password Tidak Sama'})
+    }
+    
+})
+
+
+routes.post('/proses-kategori' , (req,res) => {
+
+    connection.query('INSERT INTO kategori (Nama) values (?)', 
+    [ req.body.Nama ], 
+    function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            console.log(rows)
+            res.redirect('/kategoris')
+        }
+    });
+})
+
+routes.get('/hapus-kategori/:id' , (req,res) => {
+
+    connection.query('DELETE FROM kategori WHERE id = (?)', 
+    [ req.params.id ], 
+    function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            console.log(rows)
+            res.redirect('/kategoris')
+        }
+    });
+})
+
+routes.get('/hapus-blog/:id' , (req,res) => {
+
+    connection.query('DELETE FROM blogs WHERE id = (?)', 
+    [ req.params.id ], 
+    function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            console.log(rows)
+            res.redirect('/tutorial')
+        }
+    });
 })
 
 
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+const session = require('express-session')
 
 app.set('view engine', 'ejs')
 
+app.use(session({
+    secret: 'rahasia'
+}))
+app.use(function(req, res, next) {
+    res.locals.kunci = req.session.kunci;
+    next();
+})
 app.use(express.urlencoded({ extended: true })) 
 app.use(express.json())
 app.use(express.static('public'))
